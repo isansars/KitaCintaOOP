fc2bd29859069d5959d9b2ab5c5dab65127aa23a
Madiha <madiha.ainayya@gmail.com>
2020-03-22 22:55:44 +0700

    gui

--- src/DemoGrid.java
+++ src/DemoGrid.java
@@ -1,10 +1,14 @@
 import java.awt.*;  
-import javax.swing.*;  
+import javax.swing.*; 
+import java.util.Scanner; 
 
 public class DemoGrid {
     public static void main(String[] args) {  
+        Scanner input = new Scanner(System.in);
+
         GridField x = new GridField();
         //x.editGrid("P", 13);
-        x.editGrid("P", 3);
+        int n = input.nextInt();
+        x.editGrid("P", n);
     }  
 }
