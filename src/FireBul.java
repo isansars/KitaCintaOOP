38593e642e479bd19ef9eae13065e011afce649e
isansars <ichsansar336@gmail.com>
2020-03-29 14:39:09 +0700

    update NormalBul, FireBul

--- src/FireBul.java
+++ src/FireBul.java
@@ -1,7 +1,7 @@
 public class FireBul extends Bullet{
     // turunan Bullet dengan speed = 2, damage = 3, dan bulletType = "o" .
 
-    public FireBul(int plantPosition, GridField grid, ArrayList<Bullet> arr){
+    public FireBul(int plantPosition, GridField grid, EntityArray<Bullet> arr){
         super("o", 3, 2, plantPosition, grid, arr);
     }
 }
