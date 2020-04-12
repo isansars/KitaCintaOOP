12943188e39469ec83b92c6b29eade4807126f52
isansars <ichsansar336@gmail.com>
2020-04-12 18:31:39 +0700

    Update GridField.java

--- src/GridField.java
+++ src/GridField.java
@@ -9,10 +9,15 @@ public class GridField{
     // Atribut  
     private JFrame f;
     private JButton[] arrayButton;
+    private ButtonGroup buyButton;
       
     public GridField(){  
         f = new JFrame();
         arrayButton = new JButton[60];
+        buyButton = new ButtonGroup();
+
+        // Set Title
+        f.setTitle("Plants vs. Zombies - Kita Cinta OOP");
 
         // Insert into array
         int i;
@@ -25,8 +30,41 @@ public class GridField{
             f.add(arrayButton[i]);
         }
       
-        f.setLayout(new GridLayout(4,15));  
         //setting grid layout of 4 rows and 7 columns  
+        f.setLayout(new GridLayout(4,15));  
+
+        // Buat Plant Button - BUY
+        // Plant A
+        JButton paButton = new JButton( new AbstractAction("Plant A") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                // add Action
+            }
+        });
+        paButton.setText("PA");
+        buyButton.add(paButton);
+        f.add(paButton);
+        
+        // Plant B
+        JButton pbButton = new JButton( new AbstractAction("Plant B") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                // add Action
+            }
+        });
+        pbButton.setText("PB");
+        buyButton.add(pbButton);
+        f.add(pbButton);
+
+        
+        /* buyButton.setBounds(130,100,100, 40);
+
+        JButton addButton = new JButton( new AbstractAction("add") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                // add Action
+            }
+        }); */
       
         f.setSize(1000,400);  
         f.setVisible(true);  
@@ -36,6 +74,14 @@ public class GridField{
     public String getTextButton(int i) {
         return arrayButton[i].getText();
     }
+
+    /* @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button1){
+        //Do Something
+        }else if(e.getSource() == button2){
+        //Do Something Else
+    } */
     
     public void editGrid(String code, int position) {
     // position adalah index dari grid yang ingin diubah, code adalah karakter yang akan muncul di grid tsb
