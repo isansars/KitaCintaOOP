cc3edd1a2dd56b071928be88300660acce491b2a
isansars <ichsansar336@gmail.com>
2020-04-19 18:23:38 +0700

    delete BulletDemo.java, DemoGrid.java, ZombieDemo.java

--- src/BulletDemo.java
+++
@@ -1,9 +0,0 @@
-public class BulletDemo {
-    public static void main(String[] args) {
-
-        GridField g = new GridField();
-        EntityArray<Zombie> arrZ = new EntityArray<Zombie>();
-        EntityArray<Plant> arrP = new EntityArray<Plant>();
-        EntityArray<Bullet> arrB = new EntityArray<Bullet>();
-    }
-}
